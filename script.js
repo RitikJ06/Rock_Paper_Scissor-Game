b3c94577ecd0818839cfa437b121c13226a9a590
Ritik Jangid <ritikjangir.06@gmail.com>
2023-03-11 07:42:17 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// define function to open rules popup
+function closeRules() {
+    const rulesButton = document.getElementById("rules");
+    rulesButton.style.display = 'none';
+}
+// define function to close rules popup
+function openRules() {
+    const rulesButton = document.getElementById("rules");
+    rulesButton.style.display = 'flex'; 
+}
